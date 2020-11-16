a2ac95ecbe505203750ff3db080624a55755925b
Kalpesh Patel <kalpeshpatel.it@gmail.com>
2020-11-16 14:38:22 -0600

    Added devtools extension

--- src/AppStore/rootReducer.js
+++ src/AppStore/rootReducer.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import countReducer from '../AppCounter/Reducer';
 
 const rootReducer = combineReducers({
   counter: countReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
 
 export default store;
